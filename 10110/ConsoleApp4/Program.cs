26391c6ca36d4f2db553657675bc851adc2d4f45
uuu <l@abv.bg>
2020-02-09 12:09:42 +0200

    jjjj

---
+++ 10110/ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+            int c;
+            int n;
+            while (a != 0)
+            {
+                a = a / 2;
+                n = a % 2;
+Console.Write(n);
+            }
+
+            Console.WriteLine();
+            while (b != 0)
+            {
+                b = b / 2;
+                c = b % 2;
+                Console.Write(c);
+            }
+            Console.WriteLine();
+            
+             
+        }
+    }
+}
