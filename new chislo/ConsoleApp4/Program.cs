333dba39e6061a01611e3d2324679805d67b8be2
uuu <l@abv.bg>
2020-02-09 13:51:35 +0200

    oleeee

---
+++ new chislo/ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m, m1, s = 0;
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            { m = int.Parse(Console.ReadLine());
+                m1 = int.Parse(Console.ReadLine());
+                s += m % m1;
+            }
+            Console.WriteLine(s);
+
+
+        }
+    }
+}
